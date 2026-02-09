7e5b9f584c96fbe66fd44ac2a6540d0c0a6c703b
uo288764 <uo288764@uniovi.es>
2026-02-09 20:00:38 +0100

    SDI-2526-211-2.1-Creating Spring project.

---
+++ src/main/java/com/uniovi/sdi/sdi2526211labspring/Sdi2526211LabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2526211labspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2526211LabSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2526211LabSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi/sdi2526211labspring/Sdi2526211LabSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2526211labspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2526211LabSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
